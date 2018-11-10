13175886dc968da5250708844518143caef51700
izaquielnn <izaquiel.nascimento@ccc.ufcg.edu.br>
2018-11-09 23:48:30 -0300

    add Timeline logic

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,7 +1,38 @@
 import React, {Component} from 'react';
 
+import twitterlogo from '../twitter.svg';
+import './Timeline.css';
+
 export default class Timeline extends Component {
+    state = {
+        newTweet: ''
+    };
+
+    handleNewTweet = e => {
+        if (e.keyCode !== 13) return;
+        const content = this.state.newTweet;
+        const author = localStorage.getItem('@tt:username');
+        
+        console.log(content, author);
+    }
+
+    handleInputChange = e => {
+        this.setState({ newTweet: e.target.value });
+    };
+    
     render() {
-        return <h1>Timeline</h1>;
+        return (
+            <div className='timeline-wrapper'>
+                <img heigth={24} src={twitterlogo} alt='tt' />
+                <form>
+                    <textarea
+                        value={this.state.newTweet}
+                        onChange={this.handleInputChange}
+                        onKeyDown={this.handleNewTweet}
+                        placeholder="O que esta acontecendo?"
+                    />
+                </form>
+            </div>
+        );
     }
 }
